0a3ac7e09cb414a187fdfd9ffa1b2e4e66ed7f97
pdanna632 <pdanna632@gmail.com>
2024-11-13 19:19:52 -0500

    Update Marca.java

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -3,13 +3,12 @@ package taller3.televisores;
 public class Marca {
     private String nombre;
 
-
     public Marca(String nombre){
         this.nombre = nombre;
     }
 
     public String getNombre() {
-        return nombre;
+        return this.nombre;
     }
 
     public void setNombre(String newNombre) {
